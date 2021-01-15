f79a490132c39968b87094c0bf541944cfd973cb
kctribikram <kctribiking@gmail.com>
2021-01-15 16:34:19 +0545

    final commit

--- app/src/main/java/com/example/week6/Fragments/AboutFragment.kt
+++ app/src/main/java/com/example/week6/Fragments/AboutFragment.kt
@@ -5,10 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import com.example.week6.R
 
 class AboutFragment : Fragment() {
-
+private lateinit var webview:WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,8 +19,15 @@ class AboutFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+
+        val view=inflater.inflate(R.layout.fragment_about, container, false)
+
+        webview=view.findViewById(R.id.webview)
+
+        webview.loadUrl("https://softwarica.edu.np/")
+
+        return view
+
     }
 
 
